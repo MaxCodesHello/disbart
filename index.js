c49d2b7d2170360c682ba488f80758d8a0c492d6
SirEpiclyAmazing <74148830+SirEpiclyAmazing@users.noreply.github.com>
2022-01-10 18:00:50 +0000

    cleaning go brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const client = new Discord.Client();
  
 client.on("ready", () => {
   console.log("I am ready!");
-
   client.user.setActivity("use !help | servers: " + client.guilds.cache.size)
-
-
-  
 });
 
 client.on("message", (message) => {
@@ -25,13 +21,13 @@ client.on("message", (message) => {
 			.setDescription("No pings allowed")
 			.setFooter("no")
 			.setColor("RED")
-						message.channel.send(noping) 
+			message.channel.send(noping) 
 		} else {
     message.channel.send(message.content.slice(4))
 		}
   }
   if (message.content.startsWith("!greeting")) {
-    message.channel.send(`I'm disbart who the heck are you? Wait never mind, you are @${message.author.tag}. Okay well hi there! I have got a ton of features so use command !help to test them all! have fun lol!\n`);
+    message.channel.send(`I'm disbart who the heck are you? Wait never mind, you are <@${message.author.id}>. Okay well hi there! I have got a ton of features so use command !help to test them all! have fun lol!\n`);
   }
   if (message.content.startsWith("!invite")) {
     message.channel.send("Invite link: insert here also here is the link to support server insert here \n")
@@ -55,60 +51,47 @@ if(message.content.startsWith("!rob")) {
 			.setFooter("Patch made by Deveroonie and CharityLexus")
 			.setColor("RED")
 			} else {
-						message.channel.send(noping) 
+			message.channel.send(noping) 
   message.channel.send(" Help there is a robber in my house it's " + message.content.slice(4))
 			}
 }
+//DONT CHANGE BETWEEN LINE 59 AND 61
 if (message.content.startsWith("!credits")) {
     message.channel.send("Bot credits: The developer is CharityLexus#4362 but anondev, Deveroonie and MR PRinCe also helped, for more info check support server by doing !invite")
   }
+//END DONT CHANGE	
   if(message.content.startsWith("!dc")) {
-				if (message.content.includes("@")) {
+		if (message.content.includes("@")) {
 			const noping = new Discord.MessageEmbed()
 			.setTitle("Nope")
 			.setDescription("No pings allowed")
 			.setFooter("hahaha you tried")
 			.setColor("RED")
-						message.channel.send(noping) 
+			message.channel.send(noping) 
 				} else {
     message.channel.send(" Bruh i don't care. I honestly don't care about " + message.content.slice(4))
 				}
    // message.channel.send(message.content.slice(4))
   }
     if(message.content.startsWith("!imp")) {
-					if (message.content.includes("@")) {
+			if (message.content.includes("@")) {
 			const noping = new Discord.MessageEmbed()
 			.setTitle("Nope")
 			.setDescription("No pings allowed")
 			.setFooter("get trolled")
 			.setColor("RED")
-						message.channel.send(noping) 
+			message.channel.send(noping) 
 					} else {
     //message.channel.send(message.content.slice(4))
     message.channel.send(message.content.slice(4)+" was the impostor.")
-					}
+}
   }
-  
 });
 
 client.login(process.env.TOKEN);
 
-
+//FOR REPL USERS, IF YOU DONT USE REPL DELETE BELOW THIS
  var http = require('http');
-
-//create a server object, if you're not using repl.it you can remove this
-
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
-
-client.on("message", msg => {
-	if (msg.content === "!help") {
-	var interval = setInterval (function () {
-            msg.channel.send("random here")
-            .catch(console.error); // add error handling here
-        }, 
-						9999999); 
-	}
-})
+  res.write('Hello World!'); 
+}).listen(8080);
